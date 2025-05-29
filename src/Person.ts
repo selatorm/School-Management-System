d30da9d53085e0734870bd687e0a1133189e1b93
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-05-29 16:39:24 +0700

    #8: Add abstract class Person with constructor

---
+++ src/Person.ts
@@ -0,0 +1,10 @@
+export abstract class Person{
+    constructor(
+        public id: number,
+        public name: string,
+        public age: number,
+        public email: string,
+        public phoneNumber: number
+    ) {
+    }
+}
