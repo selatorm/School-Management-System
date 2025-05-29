ecd1e7010b693b94c556bcb11d8239cc28162384
sokleap <sokleap808@gmail.com>
2025-05-29 16:18:05 +0700

    create class school #2

---
+++ src/School.ts
@@ -0,0 +1,9 @@
+export class School{
+    private name: string;
+    private address: string;
+    // private classrooms: Classroom[];
+    constructor(name: string, address: string){
+        this.name = name;
+        this.address = address;
+    }
+}
