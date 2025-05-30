ea1f66923ebf5ba9b23f03d1af5d751c3bcd81d6
Sreyneath-Rom <sreyneath.rom@student.passerellesnumeriques.org>
2025-05-30 14:01:07 +0700

    fix class director,exam,admin,material,timetable

--- src/Director.ts
+++ src/Director.ts
@@ -1,15 +1,17 @@
 import { Person } from "./Person";
 
 export class Director extends Person {
-
-
-
-    constructor(id: number, name: string, age: number, email: string, phoneNumber: number) {
+    constructor(
+        id: number,
+        name: string,
+        age: number,
+        email: string,
+        phoneNumber: number
+    ) {
         super(id, name, age, email, phoneNumber);
     }
-    getRole(): string {
-        return ("I'm Director");
+
+    public getRole(): string {
+        return "I'm Director";
     }
-
-
 }
--- src/Exam.ts
+++ src/Exam.ts
@@ -1,6 +1,11 @@
 import { Student } from "./Student";
 import { Subject } from "./Subject";
 
-export class Exam{
-    constructor(private examRom:string,  private examDate:Date, private subjects:Subject, private students:Student){}
+export class Exam {
+    constructor(
+        private examRoom: string,  // Fixed typo: examRom -> examRoom
+        private examDate: Date,
+        private subject: Subject,
+        private students: Student[]
+    ) {}
 }
