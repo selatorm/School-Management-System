51af4518cdcdc247de29c56ff156f9f25d1d9d7c
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-05-30 08:33:09 +0700

    Create class Feedback and constructor .

--- src/Person.ts
+++ src/Person.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export abstract class Person {
   constructor(
     public id: number,
@@ -10,15 +9,3 @@ export abstract class Person {
 
   abstract getRole(): string;
 }
-=======
-export abstract class Person{
-    constructor(
-        public id: number,
-        public name: string,
-        public age: number,
-        public email: string,
-        public phoneNumber: number
-    ) {}
-    abstract getRole(): string;
-}
->>>>>>> 8ab83ac (Create class subject and constructor.)
