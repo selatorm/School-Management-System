3be42a3f7c2342feb340fbead73e2c12096488d6
sokleap <sokleap808@gmail.com>
2025-05-30 07:54:21 +0700

    Created class subjectType #6

---
+++ src/SubjectType.ts
@@ -0,0 +1,12 @@
+export enum SubjectType {
+    JavaScript = "JavaScript",
+    HTMLCSS = "HTMLCSS",
+    OOP = "OOP",
+    NODEJS = "Nodejs",
+    ENGLISH = "English",
+    DATABASE = "Database",
+    VUEJS = "Vuejs",
+    LARAVEL = "Laravel",
+    PHP = "PHP",
+    ALGORITHM = "Algorithm",
+}
