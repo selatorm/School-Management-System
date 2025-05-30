2c3667efa34d80d70ad2e71090317259433a6958
sokleap <sokleap808@gmail.com>
2025-05-30 07:36:15 +0700

    created class teacher #3

---
+++ src/Student.ts
@@ -0,0 +1,33 @@
+import { Person } from "./Person";
+import { Subject } from "./Subject";
+import { Assignment } from "./Assignment";
+import { Feedback } from "./Feedback";
+import { Timetable } from "./TimeTable";
+import { Grade } from "./Grades";
+
+export class Student extends Person {
+  subjects: Subject[] = [];
+  timetable: Timetable[] = [];
+  assignments: Assignment[] = [];
+  grades: Grade[] = [];
+
+  getRole(): string {
+    return "Student";
+  }
+
+  getTimetable(): Timetable[] {
+    return this.timetable;
+  }
+
+  submitAssignment(assignment: Assignment): void {
+    this.assignments.push(assignment);
+  }
+
+  getGrades(): Grade[] {
+    return this.grades;
+  }
+
+  giveFeedback(subject: Subject, rating: number, comment: string): Feedback {
+    return new Feedback(rating, comment, subject, subject.teacher);
+  }
+}
---
+++ src/Teacher.ts
@@ -0,0 +1,20 @@
+import { Person } from "./Person";
+import { Subject } from "./Subject";
+import { Material } from "./Material";
+import { Assignment } from "./Assignment";
+
+export class Teacher extends Person {
+  subjects: Subject[] = [];
+
+  getRole(): string {
+    return "Teacher";
+  }
+
+  uploadMaterial(subject: Subject, material: Material): void {
+    subject.materials.push(material);
+  }
+
+  uploadAssignment(subject: Subject, assignment: Assignment): void {
+    subject.assignments.push(assignment);
+  }
+}
